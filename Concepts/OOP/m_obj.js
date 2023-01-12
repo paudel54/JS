e2fd183d69542a8fe773f89e5f6c53825097254f
paudel54 <2sansrit@gmail.com>
2023-01-12 11:12:11 +0545

    clearified factories function

--- Concepts/OOP/m_obj.js
+++ Concepts/OOP/m_obj.js
@@ -14,4 +14,22 @@ const circle = {
     }
 };
 
-circle.draw();
+//circle.draw();
+
+//FACTORIES FUNCTION: LETS SAY WE WANT ANOTHER CIRCLE, SO WE HAVE TO DUBLICATE THE CODE.
+//DUBLICATING CREATES HUGE MESS WHEN WE HAVE LARGE NUMBER OF METHODS INSIDE PARENT OBJECT. DEBUGGIG IS MORE HECTIC
+//FACTORY FUNCTION HELPS TO RESOLVE THIS
+
+function createCircle(radius) {
+    return {
+        radius: radius,
+        draw: function () {
+            console.log('draw with radius' + this.radius);
+        }
+    };
+}
+
+const circle1 = createCircle(3);
+const circle2 = createCircle(5);
+console.log(circle1);
+console.log(circle2);
