84c80e13eb4694d0178de349f34894e715b9950d
paudel54 <2sansrit@gmail.com>
2023-01-13 13:59:31 +0545

    learn't fundamentals of promise

--- Concepts/Async/3_promise.js
+++ Concepts/Async/3_promise.js
@@ -8,4 +8,24 @@ const promise = new Promise((resolve, reject) => {
 })
 
 //we use . then to decide or access promise 
-promise.then(result => console.log(result));
+//promise.then(result => console.log(result));
+
+promise
+    .then(result => result + '!')
+    .then(result2 => result2 + '?')
+    //catch auto detect the if present error and shows onto terminal.  but the error must be present above the catch method
+    .catch(() => console.log('error'))
+    .then(result3 => {
+        console.log(result3 + '!')
+    })
+
+const promise2 = new Promise((resolve, reject) => { setTimeout(resolve, 100, 'HIII') })
+
+const promise3 = new Promise((resolve, reject) => { setTimeout(resolve, 1000, 'POOKIE') })
+
+const promise4 = new Promise((resolve, reject) => { setTimeout(resolve, 5000, 'Is it me you are looking for?') })
+
+Promise.all([promise, promise2, promise3, promise4])
+    .then(values => {
+        console.log(values);
+    })
