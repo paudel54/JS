256796985c6427cb07ffefdb899f6898f5fa9ce0
paudel54 <2sansrit@gmail.com>
2023-01-14 12:22:10 +0545

    catch block executed with syntax error in try block

--- Concepts/Error_Hand/1_error.js
+++ Concepts/Error_Hand/1_error.js
@@ -2,8 +2,10 @@
 //try { } catch { }
 
 function fail() {
+    //if we make any error inside try block, catch block gets active and executes accordingly
     try {
-        console.log('this works')
+        //whenever i made lets say syntax error in console.log with diff spelling catcch block executes and throws error
+        consoe.log('this works')
     }
 
     catch (err) {
